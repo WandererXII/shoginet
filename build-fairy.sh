8b550b33b477a157800f9954c0bb3fcac7d7f5f3
WandererXII <43043181+WandererXII@users.noreply.github.com>
2022-07-23 16:07:06 +0200

    Add script to build Fairy
    https://github.com/gbtami/fairyfishnet/blob/master/build-stockfish.sh

---
+++ build-fairy.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+echo "- Getting latest Fairy-Stockfish ..."
+
+if [ -d Fairy-Stockfish/src ]; then
+    cd Fairy-Stockfish/src
+    make clean > /dev/null
+    git pull
+else
+    git clone --depth 1 https://github.com/ianfab/Fairy-Stockfish.git
+    cd Fairy-Stockfish/src
+fi
+
+echo "- Determining CPU architecture ..."
+
+ARCH=x86-64
+EXE=stockfish-x86_64
+
+if [ -f /proc/cpuinfo ]; then
+    if grep "^flags" /proc/cpuinfo | grep -q popcnt ; then
+        ARCH=x86-64-modern
+        EXE=stockfish-x86_64-modern
+    fi
+
+    if grep "^vendor_id" /proc/cpuinfo | grep -q Intel ; then
+        if grep "^flags" /proc/cpuinfo | grep bmi2 | grep -q popcnt ; then
+            ARCH=x86-64-bmi2
+            EXE=stockfish-x86_64-bmi2
+        fi
+    fi
+fi
+
+echo "- Building $EXE ... (patience advised)"
+make build ARCH=$ARCH EXE=../../$EXE largeboards=yes > /dev/null
+
+cd ../..
+echo "- Done!"
